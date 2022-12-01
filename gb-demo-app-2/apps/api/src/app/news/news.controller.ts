b5f79e37ffc089ad9dc8e245131883986955482a
Dmitrii <shokilla@mail.ru>
2022-12-01 16:16:56 +0400

    add server cash

--- gb-demo-app-2/apps/api/src/app/news/news.controller.ts
+++ gb-demo-app-2/apps/api/src/app/news/news.controller.ts
@@ -10,34 +10,50 @@ export class CreateNewsDto {
   description: string;
 }
 
+const db = new Map();
+let resCash = [];
+
 @Controller('news')
 export class NewsController {
   @Get()
   async getNews() {
-    return new Promise(resolve => {
-      const news = Object.keys([...Array(20)])
-        .map(key => Number(key) + 1)
-        .map(n => ({
-          id: n,
-          title: `Важная новость ${n}`,
-          description: (rand => ([...Array(rand(1000))].map(() => rand(10**16).toString(36).substring(rand(10))).join(' ')))(max => Math.ceil(Math.random() * max)),
-          createdAt: Date.now()
-        }))
+    return new Promise((resolve) => {
+      if (!db.has('genNews')) {
+        const news = Object.keys([...Array(20)])
+          .map((key) => Number(key) + 1)
+          .map((n) => ({
+            id: n,
+            title: `Важная новость ${n}`,
+            description: ((rand) =>
+              [...Array(rand(1000))]
+                .map(() =>
+                  rand(10 ** 16)
+                    .toString(36)
+                    .substring(rand(10))
+                )
+                .join(' '))((max) => Math.ceil(Math.random() * max)),
+            createdAt: Date.now(),
+          }));
+
+        db.set('genNews', news);
+        resCash = Array.from(db.values()).flat();
+      }
 
       setTimeout(() => {
-        resolve(news);
-      }, 100)
+        // resolve(news);
+        resolve(resCash);
+      }, 100);
     });
   }
 
   @Post()
   @Header('Cache-Control', 'none')
   create(@Body() peaceOfNews: CreateNewsDto) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         console.log('Новость успешно создана', peaceOfNews);
         resolve({ id: Math.ceil(Math.random() * 1000), ...peaceOfNews });
-      }, 100)
+      }, 100);
     });
   }
 }
