d1452c9526c87655e58afd2a54e25799348e990e
Dmitrii <shokilla@mail.ru>
2022-12-20 14:31:03 +0400

    tested memcashed + redis

--- gb-demo-app-7/apps/api/src/app/news/news.controller.ts
+++ gb-demo-app-7/apps/api/src/app/news/news.controller.ts
@@ -1,9 +1,9 @@
 import { Body, Controller, Get, Header, Post, Param } from '@nestjs/common';
 import { IsNotEmpty } from 'class-validator';
-import memjs from 'memjs';
+// import memjs from 'memjs';
 import Redis from 'ioredis';
 
-const memcached = memjs.Client.create();
+// const memcached = memjs.Client.create();
 const redis = new Redis();
 
 export class CreateNewsDto {
@@ -18,44 +18,55 @@ export class CreateNewsDto {
 export class NewsController {
   @Get()
   async getNews() {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       const news = Object.keys([...Array(20)])
-        .map(key => Number(key) + 1)
-        .map(n => ({
+        .map((key) => Number(key) + 1)
+        .map((n) => ({
           id: n,
           title: `Важная новость ${n}`,
-          description: (rand => ([...Array(rand(1000))].map(() => rand(10**16).toString(36).substring(rand(10))).join(' ')))(max => Math.ceil(Math.random() * max)),
-          createdAt: Date.now()
-        }))
+          description: ((rand) =>
+            [...Array(rand(1000))]
+              .map(() =>
+                rand(10 ** 16)
+                  .toString(36)
+                  .substring(rand(10))
+              )
+              .join(' '))((max) => Math.ceil(Math.random() * max)),
+          createdAt: Date.now(),
+        }));
 
       setTimeout(() => {
         resolve(news);
-      }, 100)
+      }, 100);
     });
   }
 
   @Post()
   @Header('Cache-Control', 'none')
   create(@Body() peaceOfNews: CreateNewsDto) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         console.log('Новость успешно создана', peaceOfNews);
         resolve({ id: Math.ceil(Math.random() * 1000), ...peaceOfNews });
-      }, 100)
+      }, 100);
     });
   }
 
-  @Get('test-memcached/:searchtext')
-  async testMemcached(@Param('searchtext') searchtext: string) {
-    memcached.set("foo", searchtext, 10);
+  // @Get('test-memcached/:searchtext')
+  // async testMemcached(@Param('searchtext') searchtext: string) {
+  //   memcached.set('foo', searchtext, { expires: 600 });
 
-    return await memcached.get("foo").then(a => a.value.toString());
-  }
+  //   const result = await memcached.get('foo');
 
-  @Get('test-redis/:searchtext')
-  async testRedis(@Param('searchtext') searchtext: string) {
-    redis.set("foo", searchtext);
+  //   return result.value.toString();
+  // }
 
-    return await redis.get("foo");
-  }
+  // @Get('test-redis/:searchtext')
+  // async testRedis(@Param('searchtext') searchtext: string) {
+  //   redis.set('foo', searchtext);
+
+  //   const result = await redis.get('foo');
+
+  //   return result;
+  // }
 }
