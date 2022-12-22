a069efa369c30f47ce2e40ce1ca3212842eb0868
Dmitrii <shokilla@mail.ru>
2022-12-22 12:26:33 +0400

    added redis cashe

--- gb-demo-app-7/apps/api/src/app/news/news.controller.ts
+++ gb-demo-app-7/apps/api/src/app/news/news.controller.ts
@@ -6,6 +6,7 @@ import Redis from 'ioredis';
 
 // const memcached = memjs.Client.create();
 const redis = new Redis();
+// redis.on('error', (err) => console.log('Redis Client Error', err));
 
 export class CreateNewsDto {
   @IsNotEmpty()
@@ -20,77 +21,119 @@ let cashe;
 export class NewsController {
   @Get()
   async getNews() {
+    // await redis.connect();
     if (!cashe) {
-      const result = new Promise((resolve) => {
-        const news = Object.keys([...Array(20)])
-          .map((key) => Number(key) + 1)
-          .map((n) => ({
-            id: n,
-            title: `Важная новость ${n}`,
-            description: ((rand) =>
-              [...Array(rand(1000))]
-                .map(() =>
-                  rand(10 ** 16)
-                    .toString(36)
-                    .substring(rand(10))
-                )
-                .join(' '))((max) => Math.ceil(Math.random() * max)),
-            createdAt: Date.now(),
-          }));
+      const news = Object.keys([...Array(20)])
+        .map((key) => Number(key) + 1)
+        .map((n) => ({
+          id: n,
+          title: `Важная новость ${n}`,
+          description: ((rand) =>
+            [...Array(rand(1000))]
+              .map(() =>
+                rand(10 ** 16)
+                  .toString(36)
+                  .substring(rand(10))
+              )
+              .join(' '))((max) => Math.ceil(Math.random() * max)),
+          createdAt: Date.now(),
+        }));
+      // await news.forEach((oneNews) => {
+      //   console.log('set');
 
-        resolve(news);
-        // setTimeout(() => {
-        //   resolve(news);
-        // }, 100);
-      });
-      await redis.set('cashe', result);
-      // cashe = await redis.get('cashe');
-      cashe = new Promise((resolve, rejects) => {
-        resolve(result);
-      });
+      //   redis.set('key', JSON.stringify(oneNews));
+      // });
+      redis.set('key', JSON.stringify(news));
+      cashe = await redis.get('key');
+      console.log('test: ', cashe);
 
-      cashe.then((result) => {
-        console.log('result: ', result);
-      });
-      console.log('cashe: ', cashe);
-      return result;
+      return news;
     } else {
-      console.log('hohohoho');
-
       return cashe;
     }
-    // const result = new Promise((resolve) => {
-    //   const news = Object.keys([...Array(20)])
-    //     .map((key) => Number(key) + 1)
-    //     .map((n) => ({
-    //       id: n,
-    //       title: `Важная новость ${n}`,
-    //       description: ((rand) =>
-    //         [...Array(rand(1000))]
-    //           .map(() =>
-    //             rand(10 ** 16)
-    //               .toString(36)
-    //               .substring(rand(10))
-    //           )
-    //           .join(' '))((max) => Math.ceil(Math.random() * max)),
-    //       createdAt: Date.now(),
-    //     }));
-
-    //   resolve(news);
-    //   // setTimeout(() => {
-    //   //   resolve(news);
-    //   // }, 100);
-    // });
-    // return result;
   }
 
+  // @Get()
+  // async getNews() {
+  //   if (!cashe) {
+  //     const result = new Promise((resolve) => {
+  //       const news = Object.keys([...Array(20)])
+  //         .map((key) => Number(key) + 1)
+  //         .map((n) => ({
+  //           id: n,
+  //           title: `Важная новость ${n}`,
+  //           description: ((rand) =>
+  //             [...Array(rand(1000))]
+  //               .map(() =>
+  //                 rand(10 ** 16)
+  //                   .toString(36)
+  //                   .substring(rand(10))
+  //               )
+  //               .join(' '))((max) => Math.ceil(Math.random() * max)),
+  //           createdAt: Date.now(),
+  //         }));
+
+  //       resolve(news);
+  //       // setTimeout(() => {
+  //       //   resolve(news);
+  //       // }, 100);
+  //     });
+  //     await redis.set('cashe', result);
+
+  //     // cashe = await redis.get('cashe');
+
+  //     cashe = await new Promise((resolve, rejects) => {
+  //       resolve(redis.get('cashe'));
+  //       // resolve(result);
+  //     });
+
+  //     // cashe.then((result) => {
+  //     //   console.log('result: ', result);
+  //     // });
+  //     console.log('cashe: ', cashe);
+  //     return result;
+  //   } else {
+  //     console.log('hohohoho');
+
+  //     return cashe;
+  //   }
+  // const result = new Promise((resolve) => {
+  //   const news = Object.keys([...Array(20)])
+  //     .map((key) => Number(key) + 1)
+  //     .map((n) => ({
+  //       id: n,
+  //       title: `Важная новость ${n}`,
+  //       description: ((rand) =>
+  //         [...Array(rand(1000))]
+  //           .map(() =>
+  //             rand(10 ** 16)
+  //               .toString(36)
+  //               .substring(rand(10))
+  //           )
+  //           .join(' '))((max) => Math.ceil(Math.random() * max)),
+  //       createdAt: Date.now(),
+  //     }));
+
+  //   resolve(news);
+  //   // setTimeout(() => {
+  //   //   resolve(news);
+  //   // }, 100);
+  // });
+  // return result;
+  // }
   @Post()
   @Header('Cache-Control', 'none')
   create(@Body() peaceOfNews: CreateNewsDto) {
     return new Promise((resolve) => {
       setTimeout(() => {
         console.log('Новость успешно создана', peaceOfNews);
-        resolve({ id: Math.ceil(Math.random() * 1000), ...peaceOfNews });
+        const newNews = resolve({
+          id: Math.ceil(Math.random() * 1000),
+          ...peaceOfNews,
+        });
+        console.log('newNews: ', newNews);
+
+        redis.set('cashe', newNews);
       }, 100);
     });
   }
@@ -104,12 +147,13 @@ export class NewsController {
   //   return result.value.toString();
   // }
 
-  // @Get('test-redis/:searchtext')
-  // async testRedis(@Param('searchtext') searchtext: string) {
-  //   redis.set('foo', searchtext);
+  @Get('test-redis/:searchtext')
+  async testRedis(@Param('searchtext') searchtext: string) {
+    redis.set('foo', searchtext);
 
-  //   const result = await redis.get('foo');
+    const result = await redis.get('foo');
+    console.log('result', result);
 
-  //   return result;
-  // }
+    return result;
+  }
 }
